fccf2f224be5e27c0f2db64f6685911c4102d8d1
rebiss <barsegyanat.93@gmail.com>
2020-12-30 21:57:28 +0400

    add: server, front part

--- rc-draw/src/components/App.jsx
+++ rc-draw/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import {Canvas} from '../components/Canvas/canvas.component'
 import {SettingBar} from '../components/SettingBar/settingBar.component'
 import {ToolBar} from '../components/ToolBar/toolBar.component'
@@ -7,10 +7,17 @@ import './assets/app.scss'
 
 export const App = () => {
   return (
-    <div className='app'>
-      <Canvas />
-      <SettingBar />
-      <ToolBar />
-    </div>
+    <BrowserRouter> 
+      <div className='app'>
+        <Switch>
+          <Route path='/:id'>
+            <Canvas />
+            <SettingBar />
+            <ToolBar />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+
   )
 }
